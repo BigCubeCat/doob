c37be82aba540d20b2f2d9eb8a7bf90481a9e504
BigCubeCat <bigcubecat@yandex.ru>
2022-06-24 22:44:52 +0700

    logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 
 
 export default function Header({ user, logout }) {
@@ -18,7 +17,7 @@ export default function Header({ user, logout }) {
                         aria-label="menu"
                         sx={{mr: 2}}
                     >
-                        <MenuIcon/>
+                        <img src='/logo.png' height='48px' alt="d(o.o)b" />
                     </IconButton>
                     <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
                         d(o.o)b
