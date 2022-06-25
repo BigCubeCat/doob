c98a5e67885215ccc3cd5a50a96741927a2d1e62
BigCubeCat <bigcubecat@yandex.ru>
2022-06-25 11:06:15 +0700

    fix colors

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -1,25 +1,30 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import {HEX_COLORS, COLOR_BUTTON_SIZE} from '../consts';
 
 export default function Colors() {
   return (
-      <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-      >
-        <Box className="blockContainer">
-          <Box className="bloc" style={{backgroundColor: '#f22f23'}}/>
-          <Box className="bloc" style={{backgroundColor: '#f2de00'}}/>
-          <Box className="bloc" style={{backgroundColor: '#1e9773'}}/>
-          <Box className="bloc" style={{backgroundColor: '#004984'}}/>
-          <Box className="bloc" style={{backgroundColor: '#231f21'}}/>
-          <Box className="bloc" style={{backgroundColor: '#99938f'}}/>
-          <Box className="bloc" style={{backgroundColor: '#d32481'}}/>
-          <Box className="bloc" style={{backgroundColor: '#c65223'}}/>
-        </Box>
-      </Box>
+      <Grid sx={{flexGrow: 1}} container spacing={2}>
+        {[0, 1].map((value) => (
+            <Grid item xs={12}>
+              <Grid container justifyContent="center" spacing={2}>
+                {[0, 1, 2, 3].map((val) => (
+                    <Grid key={val} item>
+                      <Paper
+                          sx={{
+                            height: COLOR_BUTTON_SIZE,
+                            width: COLOR_BUTTON_SIZE,
+                            backgroundColor: () =>
+                                HEX_COLORS[value * 3 + val],
+                          }}
+                      />
+                    </Grid>
+                ))}
+              </Grid>
+            </Grid>
+        ))}
+
+      </Grid>
   );
 }
