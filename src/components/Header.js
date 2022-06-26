20564351b051e7c80b200d3ff88539b3c07942db
KXRXH <kirayfolon@gmail.com>
2022-06-26 15:03:05 +0700

    com

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ export default function Header({user, logout, setNotAuth}) {
   const isCartoons = useSelector(state => state).is_cartoons;
   return (
 
-      <AppBar position="static" style={{background: isCartoons ? "#cf1302" : '#5edfff'}}>
+      <AppBar position="static" style={{background: isCartoons ? "#cf1302" : '#1DB954'}}>
         <Toolbar>
           <Switch color="default" onClick={() => {
             dispatch(setIsCartoons(true));
